a11daa44ce44ce7b102ccd825164a4c29a63fc03
BaeJuh <ghkslqlsl04@gmail.com>
2025-08-01 17:15:51 +0900

    update tabulation

--- PrefixSum/src/Main.java
+++ PrefixSum/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        int[] coins = {1, 2, 5};
-        int change = 5;
+        int[] coins = {2, 3, 5, 10};
+        int change = 10;
 //        int change = 540;
 
         int events = changeEventCounter(coins, change);
